7985966465c2ad6231a96244287e5e06dfe6b6c4
Devesh Soni <deveshsoni973@gmail.com>
2025-12-22 16:00:46 +0530

    Added user profiles for tech team

--- backend/app/models/users.py
+++ backend/app/models/users.py
@@ -1,6 +1,14 @@
 from pydantic import BaseModel, EmailStr
 
 
+from enum import Enum
+
+class Role(str, Enum):
+    USER = "USER"
+    ADMIN = "ADMIN"
+    SUPER_ADMIN = "SUPER_ADMIN"
+
+
 class UserCreate(BaseModel):
     name: str
     email: EmailStr
@@ -16,6 +24,7 @@ class UserOut(BaseModel):
     id: str
     name: str
     email: str
+    role: Role
 
     class Config:
         from_attributes = True
